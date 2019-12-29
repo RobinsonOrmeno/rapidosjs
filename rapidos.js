30924f799055b32a60d42cbcfd51dac169f02ad7
Robinson <r.ormenomeza@gmail.com>
2019-12-29 00:49:34 -0300

    primero

---
+++ rapidos.js
@@ -0,0 +1,15 @@
+/*
+callbacks eventos y promesas
+workers : dos procesos distintos corriendo en paralelo
+dom manipulation
+
+typescript compilador al igual que sass 
+
+Angular y java
+
+React y js
+
+VueJS comunidad
+
+
+*/
