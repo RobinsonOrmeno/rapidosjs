15e0666c3dbb40c5c34138d3cdb12dcd252dba50
Robinson <r.ormenomeza@gmail.com>
2019-12-31 19:23:56 -0300

    asyncyawait

---
+++ asyncAwait.js
@@ -0,0 +1,41 @@
+const getNombre = (idPost) =>{
+        fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`)
+           .then(res =>{ 
+               return res.json()
+        })
+        .then(post =>{
+            
+            console.log(post.id);
+            fetch(`https://jsonplaceholder.typicode.com/users/${post.id}`)
+                .then(res =>{
+                    return res.json()
+                })
+                    .then(user =>{
+                        console.log(user.name)
+                    })
+        })
+}
+
+
+//getNombre(6);
+
+
+
+
+const getNombreAsync = async(idPost)=>{
+    try {
+        const resPost = await fetch(`https://jsonplaceholder.typicode.com/posts/${idPost}`)
+
+        const post = await resPost.json()
+        console.log(post)
+
+        const resUser = await  fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`)
+        const user = await resUser.json()
+        console.log(user.name)
+      
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getNombreAsync(18)
