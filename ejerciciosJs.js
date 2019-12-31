3bd363338c43ac60cd2a3048a9dfde7796292a31
Robinson <r.ormenomeza@gmail.com>
2019-12-31 17:13:07 -0300

    spread

--- ejerciciosJs.js
+++ ejerciciosJs.js
@@ -46,3 +46,42 @@ let operadorPropagacion =() =>{
 
 
 }
+
+/*
+SPREAD
+
+*/
+
+
+function myfunction(x,y){
+    return x + y;
+}
+
+var data = [1,2];
+
+var result = myfunction(...data)
+
+console.log(result);
+
+
+// let array1 = [1,2,3]
+// let array2 = [0,...array1,4,5]
+
+console.log(array2)
+
+
+let arrayUno = [2,3,4]
+let arrayDos = [1]
+arrayDos.push(...arrayUno)
+console.log(arrayDos)
+
+
+let array1 = [1]
+let array2 = [2]
+let array3 = [...array1,...array2,...[3,4]]
+let array4 = [5]
+let spreadsArray = (a,b,c,d,e)=>{
+    return a+b+c+d+e
+}
+let result = spreadsArray(...array3,...array4)
+console.log(result)
